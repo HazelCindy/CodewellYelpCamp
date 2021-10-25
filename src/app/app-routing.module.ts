9bd1ab24940ab27e5b6547f7b0195da7bc30f1db
HazelCindy <akinyiagnes.97@gmail.com>
2021-10-25 17:46:13 +0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { LandingComponent } from './landing/landing.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'landing', component: LandingComponent },
+  {
+    path: '', component: LandingComponent
+  },
+  // { path: '',   redirectTo: '/landing', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
